d156c74353a463f8d615678da756800d0921d02b
peaerfl <peaerfl@gmail.com>
2018-12-26 18:44:28 +0200

    dfxf

--- BL/BL_imp.cs
+++ BL/BL_imp.cs
@@ -22,9 +22,11 @@ namespace BL
 
             }
        
-        void remove_tester(int _id);
+        void remove_tester(int _id)
+        { }
        
-        void update_tester(int _id, string _lastname, string _firstname, DateTime _date_of_birth, gender _Gender, int _phone, Address _address, float _expirence, int _max_testPerWeek, vehicle _tester_expertise);
+        void update_tester(int _id, string _lastname, string _firstname, DateTime _date_of_birth, gender _Gender, int _phone, Address _address, float _expirence, int _max_testPerWeek, vehicle _tester_expertise)
+        { }
        
         void add_trainee(int _id, string _last_name, string _first_name, DateTime _date_of_birth, gender _Gender, int _phone, Address _address, vehicle _learn_vehicle, kind_of_gearbox _gearbox, string _school, string _teacher_name, int _numOfLessons)
             {
@@ -34,22 +36,29 @@ namespace BL
                  {
                      throw new Exception("Trainee must be  at least 18 years old.");
                  }
-                effector.add_trainee( _id,  _lastname,  _firstname,  _date_of_birth,  _Gender,  _phone,  _address,  _expirence,  _max_testPerWeek,  _tester_expertise,_max_way);
+                effector.add_trainee( _id,  _last_name,  _first_name,  _date_of_birth,  _Gender,  _phone,  _address,  _learn_vehicle,  _gearbox, _school, _teacher_name,_numOfLessons);
             }
 
         
-        void remove_trainee(int _id);
+        void remove_trainee(int _id)
+        { }
      
-        void update_trainee(int _id, string _last_name, string _first_name, DateTime _date_of_birth, gender _Gender, int _phone, Address _address, vehicle _learn_vehicle, kind_of_gearbox _gearbox, string _school, string _teacher_name, int _numOfLessons);
+        void update_trainee(int _id, string _last_name, string _first_name, DateTime _date_of_birth, gender _Gender, int _phone, Address _address, vehicle _learn_vehicle, kind_of_gearbox _gearbox, string _school, string _teacher_name, int _numOfLessons)
+        { }
      
-        void add_test(int _id_tester, int _id_trainee, DateTime _dateAndHour, Address _address);
+        void add_test(int _id_tester, int _id_trainee, DateTime _dateAndHour, Address _address)
+        { }
      
-        void update_test(int id, int id_tester, int id_trainee, DateTime date, Address address, bool distance, bool reverse, bool mirrors, bool signals, bool grade, string mention);
+        void update_test(int id, int id_tester, int id_trainee, DateTime date, Address address, bool distance, bool reverse, bool mirrors, bool signals, bool grade, string mention)
+        { }
    
-        List<Tester> all_tester();
+        List<Tester> all_tester()
+        { }
      
-        List<Trainee> all_trainee();
+        List<Trainee> all_trainee()
+        { }
      
-        List<Test> all_test(); 
+        List<Test> all_test()
+        { }
     }
 }

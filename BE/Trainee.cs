8f4a77013875adedfee7bdcd95942b9ebe15a247
peaerfl <peaerfl@gmail.com>
2018-12-26 15:12:15 +0200

    ij

--- BE/Trainee.cs
+++ BE/Trainee.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace BE
 {
-    //fisdlfkd
     public class Trainee
     {
         int _id;
@@ -21,6 +20,7 @@ namespace BE
         string _school;
         string _teacher_name;
         int _numOfLessons;
+        //knlknlk
 
         public Trainee(int id, string last_name, string first_name, DateTime date_of_birth, gender Gender, int phone, Address address, vehicle learn_vehicle, kind_of_gearbox gearbox, string school, string teacher_name, int numOfLessons)
         {

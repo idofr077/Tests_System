19932b5814e9e3c7224217d1175fbb039121b765
ido frideman <idofr077@gmail.com>
2018-12-30 16:51:16 +0200

    j

--- BL/BL_imp.cs
+++ BL/BL_imp.cs
@@ -8,10 +8,10 @@ using BE;
 
 namespace BL
 {
-   public class BL_imp : IBL
+    public class BL_imp : IBL
     {
         Idal effector = new Dal_imp();
-      public  void add_tester(int _id, string _lastname, string _firstname, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, float _expirence, int _max_testPerWeek, vehicle _tester_expertise,bool [,] _work_time ,int _max_way)
+        public void add_tester(int _id, string _lastname, string _firstname, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, float _expirence, int _max_testPerWeek, vehicle _tester_expertise, bool[,] _work_time, int _max_way)
         {
             TimeSpan Temp;
             Temp = DateTime.Now - _date_of_birth;
@@ -19,11 +19,11 @@ namespace BL
             {
                 throw new Exception("Tetser must be  at least 40 years old.");
             }
-            effector.add_tester(_id, _lastname, _firstname, _date_of_birth, _Gender, _phone, _address, _expirence, _max_testPerWeek, _tester_expertise,_work_time, _max_way);
+            effector.add_tester(_id, _lastname, _firstname, _date_of_birth, _Gender, _phone, _address, _expirence, _max_testPerWeek, _tester_expertise, _work_time, _max_way);
 
         }
 
-       public  void remove_tester(int _id)
+        public void remove_tester(int _id)
         {
             if (!effector.id_alredy_exsits(_id))
             {
@@ -32,16 +32,16 @@ namespace BL
             effector.remove_tester(_id);
         }
 
-       public void update_tester(int _id, string _lastname, string _firstname, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, float _expirence, int _max_testPerWeek, vehicle _tester_expertise)
+        public void update_tester(int _id, string _lastname, string _firstname, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, float _expirence, int _max_testPerWeek, vehicle _tester_expertise)
         {
             if (!effector.id_alredy_exsits(_id))
             {
                 throw new Exception("the id is not exsits");
             }
-            effector.update_tester(_id, _lastname,  _firstname,  _date_of_birth,  _Gender,  _phone,  _address,  _expirence,  _max_testPerWeek,  _tester_expertise);
+            effector.update_tester(_id, _lastname, _firstname, _date_of_birth, _Gender, _phone, _address, _expirence, _max_testPerWeek, _tester_expertise);
         }
 
-       public void add_trainee(int _id, string _last_name, string _first_name, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, vehicle _learn_vehicle, kind_of_gearbox _gearbox, string _school, string _teacher_name, int _numOfLessons)
+        public void add_trainee(int _id, string _last_name, string _first_name, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, vehicle _learn_vehicle, kind_of_gearbox _gearbox, string _school, string _teacher_name, int _numOfLessons)
         {
             TimeSpan Temp;
             Temp = DateTime.Now - _date_of_birth;
@@ -61,18 +61,18 @@ namespace BL
             effector.remove_trainee(_id);
         }
 
-       public void update_trainee(int _id, string _last_name, string _first_name, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, vehicle _learn_vehicle, kind_of_gearbox _gearbox, string _school, string _teacher_name, int _numOfLessons)
+        public void update_trainee(int _id, string _last_name, string _first_name, DateTime _date_of_birth, gender _Gender, long _phone, Address _address, vehicle _learn_vehicle, kind_of_gearbox _gearbox, string _school, string _teacher_name, int _numOfLessons)
         {
             if (!effector.id_alredy_exsits(_id))
             {
                 throw new Exception("the id is not exsits");
             }
-            effector.update_trainee( _id, _last_name,  _first_name,  _date_of_birth,  _Gender,  _phone,  _address,  _learn_vehicle,  _gearbox,  _school,  _teacher_name,  _numOfLessons);
+            effector.update_trainee(_id, _last_name, _first_name, _date_of_birth, _Gender, _phone, _address, _learn_vehicle, _gearbox, _school, _teacher_name, _numOfLessons);
         }
 
-       public void add_test(int _id_tester, int _id_trainee, DateTime _dateAndHour, Address _address)
+        public void add_test(int _id_tester, int _id_trainee, DateTime _dateAndHour, Address _address)
         {
-            
+
             Trainee trainee = effector.trainee_by_id(_id_trainee);
             Tester tester = effector.tester_by_id(_id_tester);
             if (trainee.numOfLessons <= 20)
@@ -88,7 +88,7 @@ namespace BL
             if (tester.Tests_Determined.Exists(X => X == _dateAndHour))
                 throw new Exception("the date and time are not avialible .");
             var test_on_week = from DateTime item in tester.Tests_Determined
-                               let a= (DateTime.Now - item).Days
+                               let a = (DateTime.Now - item).Days
                                where (a < 7)
                                select item;
             if (test_on_week.Count() >= tester.max_testPerWeek)
@@ -99,7 +99,7 @@ namespace BL
             effector.add_test(_id_tester, _id_trainee, _dateAndHour, _address);
         }
 
-     public   void update_test(int id, int id_tester, int id_trainee, DateTime date, Address address, bool distance, bool reverse, bool mirrors, bool signals, bool crosswalk, bool grade, string mention)
+        public void update_test(int id, int id_tester, int id_trainee, DateTime date, Address address, bool distance, bool reverse, bool mirrors, bool signals, bool crosswalk, bool grade, string mention)
         {
             if (!effector.id_tests_exsits(id))
             {
@@ -147,22 +147,23 @@ namespace BL
                     throw new Exception("alredy have license to this vehicle");
             }
 
+            
 
             effector.update_test(id, id_tester, id_trainee, date, address, distance, reverse, mirrors, signals, crosswalk, grade, mention);
             trainee.waiting_for_test = false;
         }
 
-      public  List<Tester> all_tester()
+        public List<Tester> all_tester()
         {
             return effector.all_tester();
         }
 
-     public   List<Trainee> all_trainee()
+        public List<Trainee> all_trainee()
         {
             return effector.all_trainee();
         }
 
-      public  List<Test> all_test()
+        public List<Test> all_test()
         {
             return effector.all_test();
         }
@@ -174,6 +175,6 @@ namespace BL
         {
             return null;
         }
-        
+
     }
 }

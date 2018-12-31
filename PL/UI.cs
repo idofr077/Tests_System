f44ee7b9e8c45d628794efab643453924da142dd
ido frideman <idofr077@gmail.com>
2018-12-31 19:49:35 +0200

    v

--- PL/UI.cs
+++ PL/UI.cs
@@ -11,7 +11,8 @@ namespace SimpleUI
     class UI
     {
         private static IBL bl = BL.FactoryBl.getBl();
-        int inputNum()
+
+        private static int InPutNum()
         {
             int x;
             Console.WriteLine("0-quit \n 1-add trainee \n 2-add tester \n 3-add test \n 4-update trainee \n 5-update tester \n 6-update test \n 7-all trainees \n 8-all testers \n 9-all tests \n 10-testers in area \n 11-testers available on time \n 12- numner of tests for trainee \n 13-to check if a trainee have a license \n 14-to get all the test by a date \n 15-all testers by expertice \n 16-all trainees by school \n 17-all trainees by teacher \n 18-all trainees by tests number \n 19-all tests that done ");
@@ -25,7 +26,7 @@ namespace SimpleUI
             int num;
             do
             {
-                num = inputNum();
+                num= InPutNum();
 
                 switch (num)
                 {
@@ -33,57 +34,194 @@ namespace SimpleUI
                         Console.WriteLine("bye!");
                         break;
                     case 1:
+                        try
+                        {
 
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine(e.Message+"\n");
+                        }
                         break;
                     case 2:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
+                        break;
                     case 3:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 4:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 5:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 6:
-                        ;
+                        try
+                        {
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 7:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 8:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
+                        break;
                     case 9:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 10:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 11:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 12:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 13:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 14:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 15:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 16:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 17:
+                        try
+                        {
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 18:
+                        try
+                        {
 
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     case 19:
+                        try
+                        {
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + "\n");
+                        }
                         break;
                     default:
                         break;

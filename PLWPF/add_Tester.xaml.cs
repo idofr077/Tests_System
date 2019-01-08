211535f92e6662b320ac4f117db629bb6c5d121b
ido frideman <idofr077@gmail.com>
2019-01-08 12:57:23 +0200

    j

--- PLWPF/add_Tester.xaml.cs
+++ PLWPF/add_Tester.xaml.cs
@@ -12,17 +12,70 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using BE;
+using BL;
 namespace PLWPF
 {
     /// <summary>
-    /// Interaction logic for add_Tester.xaml
+    /// Interaction logic for add_tester.xaml
     /// </summary>
-    public partial class add_Tester : UserControl
+    public partial class add_tester : UserControl
     {
-        public add_Tester()
+        public add_tester()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            IBL bl = BL.FactoryBl.getBl();
+            try
+            {
+                //
+                gender _Gender = BE.gender.male;
+                vehicle _expertise = BE.vehicle.private_vehicle;
+                if (gender.SelectedIndex == 0)
+                {
+                    _Gender = BE.gender.male;
+                }
+                else if (gender.SelectedIndex == 1)
+                {
+                    _Gender = BE.gender.female;
+                }
+                else if (gender.SelectedIndex == 2)
+                {
+                    _Gender = BE.gender.other;
+                }
+                if (expertise.SelectedIndex == 0)
+                {
+                    _expertise = BE.vehicle.private_vehicle;
+                }
+                else if (expertise.SelectedIndex == 1)
+                {
+                    _expertise = BE.vehicle.two_weels_vehicle;
+                }
+                else if (expertise.SelectedIndex == 2)
+                {
+                    _expertise = BE.vehicle.medium_track;
+                }
+                else if (expertise.SelectedIndex == 3)
+                {
+                    _expertise = BE.vehicle.heavy_track;
+                }
+                
+
+
+                DateTime _date = new DateTime();
+                _date = (DateTime)(date_of_birth.SelectedDate);
+
+                bl.add_trainee(int.Parse(id.Text), (string)last_name.Text, (string)first_name.Text, _date, _Gender, long.Parse(phone_number.Text), new BE.Address(street.Text, int.Parse(house_number.Text), city.Text), _Vehicle, _Gearbox, school.Text, teacher.Text, int.Parse(num_of_lessons.Text));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            */
         }
     }
 }

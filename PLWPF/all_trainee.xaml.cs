07faa1a0a4e6c3ad71047f3c9a0244b402e908f7
peaerfl <peaerfl@gmail.com>
2019-01-08 12:46:06 +0200

    dsf

--- PLWPF/all_trainee.xaml.cs
+++ PLWPF/all_trainee.xaml.cs
@@ -24,8 +24,8 @@ namespace PLWPF
         public all_trainee()
         {
             InitializeComponent();
-            List<Trainee> items = new List<Trainee>();
-            items.Add(new Trainee() { id = 2, last_name = "flyshman", first_name = "peer", phone = 0584509872, date_of_birth = DateTime.Parse("january 1, 2001"), Gender = gender.male, learn_vehicle = vehicle.heavy_track, gearbox=kind_of_gearbox.manual, num_of_test = 88,waiting_for_test=true, num_of_lessons=33, num_of_licenses=0, school="dd", teacher_name="shlomi", address = new Address("hzait", 1, "bni darom") });
+            List<Trainee> items = (BL.FactoryBl.getBl().all_trainee());
+            //items.Add(new Trainee() { id = 2, last_name = "flyshman", first_name = "peer", phone = 0584509872, date_of_birth = DateTime.Parse("january 1, 2001"), Gender = gender.male, learn_vehicle = vehicle.heavy_track, gearbox=kind_of_gearbox.manual, num_of_test = 88,waiting_for_test=true, num_of_lessons=33, num_of_licenses=0, school="dd", teacher_name="shlomi", address = new Address("hzait", 1, "bni darom") });
             lvUsers.ItemsSource = items;
 
         }

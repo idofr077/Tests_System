447e8e3931ebe92bd16bf05a67351d9b1d155cbe
peaerfl <peaerfl@gmail.com>
2019-01-28 04:54:20 +0200

    ew

--- BE/Configuration.cs
+++ BE/Configuration.cs
@@ -16,14 +16,17 @@ namespace BE
        public static int min_lessons=20;
        public static float min_AgeOfTester=40;
        public static float min_AgeOfTrainee=18;
-        static public int id_test=0;
-        public const string FILE_TRAINEE = "trainee_file.xml";
-        public const string FILE_TESTER = "tester_file.xml";
-        public const string FILE_CONFIGURATIONS = "configurations_file.xml";
+        public static  int id_test=0;
+        public const string FILE_TRAINEE = @"trainee_file.xml";
+        public static XElement trainee_root = new XElement("trainees");
+        public const string FILE_TESTER = @"tester_file.xml";
+        public static XElement testers_root = new XElement("testers");
+        public const string FILE_CONFIGURATIONS = @"configurations_file.xml";
+        public static XElement configurations_root = new XElement("configurations");
         public static class xmlsample
         {
-            public static XElement tests_root;
-            public static string tests_path = "test_file.xml";
+            public static XElement tests_root = new XElement("tests");
+            public static string tests_path = @"test_file.xml";
         }
 
         //we need to ask about the date
--- DAL/dal_xml_imp.cs
+++ DAL/dal_xml_imp.cs
@@ -12,11 +12,47 @@ namespace DAL
 {
     class dal_xml_imp : Idal
     {
+     
         public dal_xml_imp()
         {
-            int id_test = LoadFromXML<int>(Configuration.FILE_CONFIGURATIONS);
-            Test.id_Tests = id_test;
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+            Configuration.id_test = LoadFromXML<int>(Configuration.FILE_CONFIGURATIONS);
 
+        }
+        public void open_trainee_file(string path, XElement root)
+        {
+            if (!File.Exists(path))
+            {
+                root = new XElement("trainees");
+                root.Save(path);
+            }
+        }
+        public void open_tester_file(string path, XElement root)
+        {
+            if (!File.Exists(path))
+            {
+                root = new XElement("testers");
+                root.Save(path);
+            }
+        }
+        public void open_test_file(string path, XElement root)
+        {
+            if (!File.Exists(path))
+            {
+                root = new XElement("tests");
+                root.Save(path);
+            }
+        }
+        public void open_configuration_file(string path, XElement root)
+        {
+            if (!File.Exists(path))
+            {
+                root = new XElement("configurations");
+                root.Save(path);
+            }
         }
         public static void SaveToXML<T>(T source, string path)
         {
@@ -26,9 +62,11 @@ namespace DAL
             file.Close();
         }
         public static T LoadFromXML<T>(string path)
-        { FileStream file = new FileStream(path, FileMode.Open);
+        {
+            FileStream file = new FileStream(path, FileMode.Open);
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-            T result = (T)xmlSerializer.Deserialize(file); file.Close();
+            T result = (T)xmlSerializer.Deserialize(file);
+            file.Close();
             return result;
         }
 
@@ -114,8 +152,37 @@ namespace DAL
 
         public void add_test(int _id_tester, int _id_trainee, DateTime _dateAndHour, Address _address)
         {
-            Test test = new Test(Configuration.id_test, _id_tester, _id_trainee, _dateAndHour.Date, _dateAndHour, _address);
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
+            List<Tester> testers = LoadFromXML<List<Tester>>(Configuration.FILE_TESTER);
             List<Test> tests = GetTestList();
+            if (!trainees.Exists(x => x.id.CompareTo(_id_trainee) == 0))
+            {
+                throw new Exception("the id of the trainee isnt exsist");
+            }
+            if (!testers.Exists(x => x.id.CompareTo(_id_tester) == 0))
+            {
+                throw new Exception("the id of the tester isnt exsist");
+            }
+            if (tests.Exists(x => x.id.CompareTo(Configuration.id_test) == 0))
+            {
+                throw new Exception("the id is already in use");
+            }
+            Test test = new Test(Configuration.id_test, _id_tester, _id_trainee, _dateAndHour.Date, _dateAndHour, _address);
+            Trainee trainee = trainees.Find(x => x.id.CompareTo(_id_trainee) == 0);
+            trainee.LastTest = _dateAndHour.Date;
+            trainee.waiting_for_test = true;
+            Tester tester = testers.Find(x => x.id.CompareTo(_id_tester) == 0);
+            tester.Tests_Determined.Add(_dateAndHour.Date);
+            testers.RemoveAll(temp => temp.id == tester.id);
+            testers.Add(tester);
+            trainees.RemoveAll(temp => temp.id == trainee.id);
+            trainees.Add(trainee);
+            Configuration.id_test++;
             tests.Add(test);
             SaveTestListLinq(tests);
             //שורות קוד שמוסיפות מבחן
@@ -124,14 +191,35 @@ namespace DAL
 
         public void add_tester(Tester tester)
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Tester> testers = LoadFromXML<List<Tester>>(Configuration.FILE_TESTER);
+
+            if (testers.Exists(x => x.id.CompareTo(tester.id) == 0))
+            {
+                throw new Exception("the id is already in use");
+            }
+
             testers.Add(tester);
             SaveToXML<Tester>(tester, Configuration.FILE_TESTER);
         }
 
         public void add_trainee(Trainee trainee)
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
+            if (trainees.Exists(x => x.id.CompareTo(trainee.id) == 0))
+            {
+                throw new Exception("the id is already in use");
+            }
+
             trainees.Add(trainee);
             SaveToXML<Trainee>(trainee, Configuration.FILE_TRAINEE);
 
@@ -139,49 +227,120 @@ namespace DAL
 
         public List<Test> all_test()
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Test> tests = GetTestList();
             return tests;
         }
 
         public List<Tester> all_tester()
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Tester> testers = LoadFromXML<List<Tester>>(Configuration.FILE_TESTER);
             return testers;
         }
 
         public List<Trainee> all_trainee()
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
             return trainees;
         }
 
         public bool have_licenes_by_id(int _id)
         {
-            throw new NotImplementedException();
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
+            Trainee trainee = trainees.Find(x => x.id.CompareTo(_id) == 0);
+            return trainee.have_licenses;
         }
 
         public bool id_alredy_exsits(int _id)
         {
-            throw new NotImplementedException();
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
+            List<Tester> testers = LoadFromXML<List<Tester>>(Configuration.FILE_TESTER);
+
+            if (trainees.Exists(x => x.id.CompareTo(_id) == 0))
+            {
+                    return true;
+            }
+
+            if (testers.Exists(x => x.id.CompareTo(_id) == 0))
+            {
+                    return true;
+            }
+                return false;
         }
 
         public bool id_tests_exsits(int _id)
         {
-            throw new NotImplementedException();
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            List<Test> tests = GetTestList();
+            if (tests.Exists(x => x.id.CompareTo(_id) == 0))
+            {
+                return true;
+            }
+            else
+                return false;
         }
 
         public bool is_tester_available(DateTime date_and_hour, int _testser_id)
         {
-            throw new NotImplementedException();
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            Tester tester = tester_by_id(_testser_id);
+            for (int i = 0; i < tester.Tests_Determined.Count; i++)
+            {
+                if (tester.Tests_Determined[i] == date_and_hour)
+                    return false;
+            }
+            if (!((int)date_and_hour.DayOfWeek < 5 && (date_and_hour.Hour < 15) && date_and_hour.Hour > 8))
+                return false;
+            if (!(tester.work_time[(int)date_and_hour.DayOfWeek, date_and_hour.Hour - 9]))
+                return false;
+            return true;
+
         }
 
         public int num_test(Trainee trainee)
         {
-            throw new NotImplementedException();
+            return trainee.num_of_test;
         }
 
         public void remove_tester(int _id)
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Tester> testers = LoadFromXML<List<Tester>>(Configuration.FILE_TESTER);
             Tester tester = testers.Find(x => x.id.CompareTo(_id) == 0);
             testers.Remove(tester);
@@ -190,6 +349,11 @@ namespace DAL
 
         public void remove_trainee(int _id)
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
             Trainee trainee = trainees.Find(x => x.id.CompareTo(_id) == 0);
             trainees.Remove(trainee);
@@ -198,21 +362,50 @@ namespace DAL
 
         public Tester tester_by_id(int id)
         {
-            throw new NotImplementedException();
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            List<Tester> testers = LoadFromXML<List<Tester>>(Configuration.FILE_TESTER);
+            Tester tester = new Tester(testers.Find(x => x.id.CompareTo(id) == 0));
+            return tester;
+
         }
 
         public Test test_by_id(int id)
         {
-            throw new NotImplementedException();
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            List<Test> tests = GetTestList();
+            Test test = new Test(tests.Find(x => x.id.CompareTo(id) == 0));
+            return test;
+
         }
 
         public Trainee trainee_by_id(int id)
         {
-            throw new NotImplementedException();
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
+            List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
+            Trainee trainee = new Trainee(trainees.Find(x => x.id.CompareTo(id) == 0));
+            return trainee;
+
         }
 
         public void update_test(int id, int id_trainee, bool? distance, bool? reverse, bool? mirrors, bool? signals, bool? crosswalk, bool? grade, string mention)
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Test> tests = GetTestList();
             Test temp = tests.Find(x => x.id.CompareTo(id) == 0);
             temp.distance = distance;
@@ -229,6 +422,11 @@ namespace DAL
 
         public void update_tester(Tester tester)
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Tester> testers = LoadFromXML<List<Tester>>(Configuration.FILE_TESTER);
             testers.RemoveAll(temp => temp.id == tester.id);
             testers.Add(tester);
@@ -237,6 +435,11 @@ namespace DAL
 
         public void update_trainee(Trainee trainee)
         {
+            open_tester_file(Configuration.FILE_TESTER, Configuration.testers_root);
+            open_trainee_file(Configuration.FILE_TRAINEE, Configuration.trainee_root);
+            open_configuration_file(Configuration.FILE_CONFIGURATIONS, Configuration.configurations_root);
+            open_test_file(Configuration.xmlsample.tests_path, Configuration.xmlsample.tests_root);
+
             List<Trainee> trainees = LoadFromXML<List<Trainee>>(Configuration.FILE_TRAINEE);
             trainees.RemoveAll(temp => temp.id == trainee.id);
             trainees.Add(trainee);
